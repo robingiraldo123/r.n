8db30b152cd42dc0f61c64f7f1d4ec6b440ae149
robingiraldo123 <87104809+robingiraldo123@users.noreply.github.com>
2021-09-16 15:56:17 -0500

    Add files via upload

---
+++ Futbolista.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apifutbolistas.Models
+{
+    public class futbolista
+    {
+      [key]
+      public int id { get; set; }
+      public string nombre { get; set; }
+
+    public string posicion { get; set; }
+
+    public string nacionalidad { get; set; }
+
+        public string imagen { get; set; }
+    }
+}
---
+++ futbolista.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace apifutbolistas.context
+{
+    internal class futbolista
+    {
+        internal static object ToList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
